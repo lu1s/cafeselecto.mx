530a9000e7626b82b0f091fb5edaf609ef1496c0
lu1s <github@luispulido.com>
2017-04-05 12:32:07 -0700

    webhook endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/webhook', function(Request $request) {
+	return json_encode($request);
+});
